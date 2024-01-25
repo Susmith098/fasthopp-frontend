79cb0f2b20d023fa6515bdc79927b16db37908cf
Susmith A J <susmith508@gmail.com>
2024-01-25 11:45:17 +0530

    changes in validation

--- src/Validations/RegisterValidation.js
+++ src/Validations/RegisterValidation.js
@@ -1,8 +1,20 @@
 import * as Yup from "yup";
 
 export const userSchema = Yup.object({
-    name: Yup.string().required(),
-    email: Yup.string().email().required(),
-    companyName: Yup.string().required(),
-    password: Yup.string().min(4).required(),
+    name: Yup.string()
+        .required("Please enter your name")
+        .trim(),
+    email: Yup.string()
+        .matches(/^[\w.-]+@[\w.-]+\.\w+$/, "Please enter a valid email")
+        .email("Please enter a valid email")
+        .required("Please enter your email")
+        .trim(),
+    companyName: Yup.string()
+        .required("This field is required")
+        .trim(),
+    password: Yup.string()
+        .min(8, "Pasword must be 8 or more characters")
+        .max(15)
+        .nonNullable()
+        .required('Please enter your password'),
 })
